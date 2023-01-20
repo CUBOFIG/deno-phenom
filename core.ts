80f24f451eeea504151ca9fc84675fe5f1f39756
Heriberto Figueroa <figmiche@gmail.com>
2023-01-20 13:03:10 -0600

    changes/try-catch

--- core.ts
+++ core.ts
@@ -1,22 +1,26 @@
 import "https://deno.land/std@0.173.0/dotenv/load.ts";
 
 const getVideo = async () => {
-  const response = await fetch(
-    "https://api.twitch.tv/helix/videos?id=1663382590",
-    {
-      headers: {
-        "Client-Id": Deno.env.get("CLIENT_ID") || "",
-        "Authorization": Deno.env.get("TOKEN_INIT") || ""
-      },
-    }
-  );
-
-  const datajson = await response.json()
-
-  return { ...datajson, state: "offline" };
+  try {
+    const response = await fetch(
+      "https://api.twitch.tv/helix/videos?id=1663382590",
+      {
+        headers: {
+          "Client-Id": Deno.env.get("CLIENT_ID") || "",
+          "Authorization": Deno.env.get("TOKEN_INIT") || ""
+        },
+      }
+    );
+  
+    const datajson = await response.json()
+    return { ...datajson, state: "offline" };
+  } catch (error) {
+    console.log(error)
+  }
 };
 
-export const getInformation = async (): Promise<Record<string, number | string>> =>{
+export const getInformation = async () => {
+  try {
     const response = await fetch( "https://api.twitch.tv/helix/streams?user_id=144903347", {
       method: "GET",
       headers: {
@@ -28,4 +32,7 @@ export const getInformation = async (): Promise<Record<string, number | string>>
     const datajson = await response.json()
 
     return datajson.data.length <= 0 ? await getVideo() : datajson
+  } catch (error) {
+    console.log(error)
+  }
 }
